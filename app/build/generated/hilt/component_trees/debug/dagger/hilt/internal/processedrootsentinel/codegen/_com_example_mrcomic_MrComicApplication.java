c0c0c16b21522dbc5920a10f043bb6d44df51288
Leostrange <xmetalcore@outlook.com>
2025-07-04 23:39:24 -0400

    Clean up generated files and add to .gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_mrcomic_MrComicApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.mrcomic.MrComicApplication"
-)
-public final class _com_example_mrcomic_MrComicApplication {
-}
